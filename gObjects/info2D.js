0023d73786781ee61772b2db6d50ad356b465446
stéphane koehler <Stphane KOEHLER>
2018-05-04 00:20:48 +0200

    font correction

--- gObjects/info2D.js
+++ gObjects/info2D.js
@@ -57,7 +57,7 @@ function info2DDraw()
 	ctx2d.font = "Bold 15px Arial";
 	// ctx2d.fillText("Life : " + gInfo2DLifeQt*10 + "%  -  Enemies : " +  gInfo2DNbEnemies + "  -  Fps : " + gInfo2DNbFps	,10,15 );
 	ctx2d.fillText("Life : " + gInfo2DLifeQt*10 + "%  -  Enemies : " +  gInfo2DNbEnemies  + "  -  " + canvas3D.width + "x" + canvas3D.height ,10,15 );
-	ctx2d.font = "100px Arial Black";
+	ctx2d.font = "Bold 100px Arial";
 	ctx2d.globalAlpha = 0.5;
 	if(gInfo2DGameState == "Win") 	ctx2d.fillText("You Win ",canvas2D.width/2,canvas2D.height/2 );
 	if(gInfo2DGameState == "Lose") 	ctx2d.fillText("You Lose ",canvas2D.width/2,canvas2D.height/2 );
